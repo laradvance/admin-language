0e75ef3eed9529f4a6024a4145e0e9cc28a593e9
Muhlis Abdi <35951442+muhlisabdi@users.noreply.github.com>
2020-04-03 18:42:15 +0800

    Fix Login Input Icon

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -42,23 +42,25 @@
 
         @if($errors->has('username'))
           @foreach($errors->get('username') as $message)
-            <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i>{{$message}}</label><br>
+            <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i>{{ $message }}</label><br>
           @endforeach
         @endif
-
-        <input type="text" class="form-control" placeholder="{{ trans('admin.username') }}" name="username" value="{{ old('username') }}">
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        <div class="input-group col-md-12">
+          <input type="text" class="form-control" placeholder="{{ trans('admin.username') }}" name="username" value="{{ old('username') }}">
+          <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+        </div>
       </div>
       <div class="form-group has-feedback {!! !$errors->has('password') ?: 'has-error' !!}">
 
         @if($errors->has('password'))
           @foreach($errors->get('password') as $message)
-            <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i>{{$message}}</label><br>
+            <label class="control-label" for="inputError"><i class="fa fa-times-circle-o"></i>{{ $message }}</label><br>
           @endforeach
         @endif
-
-        <input type="password" class="form-control" placeholder="{{ trans('admin.password') }}" name="password">
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        <div class="input-group col-md-12">
+          <input type="password" class="form-control" placeholder="{{ trans('admin.password') }}" name="password">
+          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
       </div>
       <div class="row">
         <div class="col-xs-8">
